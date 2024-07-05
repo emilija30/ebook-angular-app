0d5dcd4945b2c8a4fa661e262b190e3a8fb901fa
emilija30 <emilijasmiljanic14@gmail.com>
2024-07-05 18:39:27 +0200

    Dodat spinner na login stranici

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {AuthService} from "../auth.service";
 import {Router} from "@angular/router";
 import {NgForm} from "@angular/forms";
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -11,31 +12,38 @@ import {NgForm} from "@angular/forms";
 export class LoginPage implements OnInit {
 
   errorMessage: string = '';
+  isLoading: boolean = false;
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router, private loadingCtrl: LoadingController) { }
 
   ngOnInit() {
   }
 
   onLogin(form: NgForm) {
-    console.log(form);
-    this.authService.login(form.value).subscribe(
-      resData => {
-        console.log("Prijava uspesna");
-        console.log(resData);
-        this.router.navigateByUrl("/");
-        this.errorMessage = ''; // Clear the error message on successful login
-
-      },
-      err => {
-        console.log('ne valja');
-        console.error(err); // log the error for debugging
-        this.errorMessage = 'Login failed. Please try again.'; // Set the error message
-
-      }
-    );
+    this.isLoading = true;
+    this.loadingCtrl
+      .create({ message: 'Please wait...' })
+      .then(loadingEl => {
+        loadingEl.present();
+        this.authService.login(form.value).subscribe(
+          resData => {
+            console.log("Prijava uspesna");
+            console.log(resData);
+            this.isLoading = false;
+            loadingEl.dismiss();
+            this.router.navigateByUrl("/");
+            this.errorMessage = ''; // Clear the error message on successful login
+          },
+          err => {
+            console.log('ne valja');
+            console.error(err); // log the error for debugging
+            this.isLoading = false;
+            loadingEl.dismiss();
+            this.errorMessage = 'Login failed. Please try again.'; // Set the error message
+          }
+        );
+      });
   }
-  
 
   toRegister(){
     this.router.navigateByUrl('register')
